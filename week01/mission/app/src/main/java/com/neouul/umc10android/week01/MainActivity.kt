d7d26c81b6d44cf299c372e325e4970d297f197a
Neouul <fluctu0q0@gmail.com>
2026-03-22 15:45:52 +0900

    feat: week01 - 미션2 로직 작성

--- week01/mission/app/src/main/java/com/neouul/umc10android/week01/MainActivity.kt
+++ week01/mission/app/src/main/java/com/neouul/umc10android/week01/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.neouul.umc10android.week01
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.toColorInt
+import com.neouul.umc10android.week01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivHappy.setOnClickListener {
+            resetColor()
+            binding.tvHappy.setTextColor("#FFEFB6".toColorInt())
         }
+
+        binding.ivExciting.setOnClickListener {
+            resetColor()
+            binding.tvExciting.setTextColor("#CEE7F5".toColorInt())
+        }
+
+        binding.ivSoso.setOnClickListener {
+            resetColor()
+            binding.tvSoso.setTextColor("#BEC3ED".toColorInt())
+        }
+
+        binding.ivUnrest.setOnClickListener {
+            resetColor()
+            binding.tvUnrest.setTextColor("#B1D3B9".toColorInt())
+        }
+
+        binding.ivAngry.setOnClickListener {
+            resetColor()
+            binding.tvAngry.setTextColor("#EB8B8B".toColorInt())
+        }
+    }
+
+    private fun resetColor() {
+        binding.tvHappy.setTextColor(Color.BLACK)
+        binding.tvExciting.setTextColor(Color.BLACK)
+        binding.tvSoso.setTextColor(Color.BLACK)
+        binding.tvUnrest.setTextColor(Color.BLACK)
+        binding.tvAngry.setTextColor(Color.BLACK)
     }
 }
