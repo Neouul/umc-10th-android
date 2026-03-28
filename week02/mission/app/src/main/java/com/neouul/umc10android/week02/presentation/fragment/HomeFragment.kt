f4673ee7388d1d8ed066dc5e91008ae615bcb52d
Neouul <fluctu0q0@gmail.com>
2026-03-28 16:59:05 +0900

    feat: week02 - 시니어 미션2 구현
    - 마지막으로 시스템 뒤로가기를 누른 후 2초안에 한번 더 누르면, Activity를 종료하는 로직 구현

--- week02/mission/app/src/main/java/com/neouul/umc10android/week02/presentation/fragment/HomeFragment.kt
+++ week02/mission/app/src/main/java/com/neouul/umc10android/week02/presentation/fragment/HomeFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.neouul.umc10android.week02.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    // 마지막으로 시스템 뒤로가기를 누른 시간을 저장할 변수
+    private var backPressedTime: Long = 0
+
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -32,6 +37,21 @@ class HomeFragment : Fragment() {
 
         // TextView에 값 설정하기
         binding.tvTitle.text = title
+
+        // 콜백 등록
+        // 시스템 뒤로가기 2번 연속으로 터치하면 Activity 종료시키기
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (System.currentTimeMillis() - backPressedTime < 2000) {
+                    // 2초 이내에 다시 누르면 액티비티 종료
+                    requireActivity().finish()
+                } else {
+                    // 처음 눌렀을 때 메시지 표시
+                    backPressedTime = System.currentTimeMillis()
+                    Toast.makeText(requireContext(), "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
